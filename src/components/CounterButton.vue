33331eaf3eacc146f84c5b7ad17a0c49f695b6ce
Panna Chow <chowpanna@gmail.com>
2020-02-08 16:34:28 +0100

    Init

---
+++ src/components/CounterButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <button class="CounterButton">{{ title }}</button>
+</template>
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
+
+<style scoped>
+.CounterButton {
+  background: pink;
+}
+</style>
